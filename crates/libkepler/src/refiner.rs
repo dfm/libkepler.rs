207607ab00e1c360bd8021473cc08638aded9230
Dan F-M <foreman.mackey@gmail.com>
2023-10-14 20:45:36 -0400

    adding another test

--- crates/libkepler/src/refiner.rs
+++ crates/libkepler/src/refiner.rs
@@ -4,7 +4,9 @@ macro_rules! refine_iterative {
         |ecc, mean_anom, ecc_anom| {
             let mut result = ecc_anom;
             for _ in 0..$max_iter {
-                let (next, diff) = kepler_householder_step!($order, $t)(ecc, mean_anom, result);
+                let (next, diff) = libkepler_householder::kepler_householder_step!($order, $t)(
+                    ecc, mean_anom, result,
+                );
                 result = result + next;
                 if diff.abs() < $tol {
                     break;
@@ -18,14 +20,22 @@ macro_rules! refine_iterative {
 #[cfg(test)]
 mod test {
     use approx::assert_abs_diff_eq;
-    use libkepler_householder::kepler_householder_step;
 
     #[test]
-    fn test_iterative() {
+    fn test_first_order_iterative() {
         let ecc = 0.65f64;
         let expected = 0.456f64;
         let mean_anom = expected - ecc * expected.sin();
         let actual = refine_iterative!(1, f64, 1e-8, 100)(ecc, mean_anom, expected);
         assert_abs_diff_eq!(actual, expected, epsilon = 1e-8);
+    }
+
+    #[test]
+    fn test_third_order_iterative() {
+        let ecc = 0.65f64;
+        let expected = 0.456f64;
+        let mean_anom = expected - ecc * expected.sin();
+        let actual = refine_iterative!(3, f64, 1e-12, 100)(ecc, mean_anom, expected);
+        assert_abs_diff_eq!(actual, expected, epsilon = 1e-12);
     }
 }
